10c9b89a182e3131522a3cbc7ca4d9659d2c0cbd
Spencer Pollock <srepollock@gmail.com>
2017-01-03 11:33:04 -0800

    :sweat: -swt Forgot to add my script files to run

---
+++ scripts/display.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output VGA-1 --mode 1280x800
+feh --bg-fill /home/spencer/Pictures/deer_minimalist.jpg
---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+$HOME/Documents/scripts/display.sh
+$HOME/Documents/scripts/polybar.sh
+$HOME/Documents/scripts/compton.sh

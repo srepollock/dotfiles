bf6c3317987c13b79021c1d1b2320f4251bc3bda
Spencer Pollock <spencer@spollock.ca>
2021-03-25 17:17:12 -0700

    Linux Setup initial commit

---
+++ scripts/install_ui.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# Install UI assets
+#
+# Setup the things I like.
+#  - Bar: i3blocks
+#  - Bar: Polybar
+#  - Terminal: 
+#  - Shell: zsh
+#  - Theme Changer: Pywal
+#  - i3lock
+#  - i3-gaps UI
+#    - update the source to look like this
+#  - Font: Terminus | Termite
+#  - File Manager: Ranger - https://github.com/ranger/ranger
+#  - Music Player: Ncmcpp + Cli-Visualizer
+#  - Music Player: Cmus - https://github.com/karistav/cava
+#  - Windows Switcher : Rofi
+#  
+#  - Inspiration: https://www.reddit.com/r/unixporn/comments/8za7uz/i3gaps_i_love_using_pywal/
+#  - Dotfiles: https://github.com/DrChef/my-dot-files
---
+++ scripts/personal_installs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Personal installs that I like
+
+# I will need to get the type of system to check the package managers. Also,
+# each system or Linux install may have different requirements. Hopefully I
+# can just setup an install though.
+#
